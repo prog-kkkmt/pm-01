0bb2d144f0079d6e01d59e4b000f98fe333baf56
kosolapovdd <64221310+kosolapovdd@users.noreply.github.com>
2020-05-21 12:52:15 +0300

    экз

---
+++ экзамен/задание 2/prog.pas
@@ -0,0 +1,17 @@
+﻿var
+  a, n, s: integer;
+
+begin
+  s := 0;
+  readln(n);
+  
+  readln(a);
+  while (n > 0) do
+  begin
+    s := s + a;
+    dec(n);
+    readln(a);
+  end;
+  
+  writeln(s);
+end.
